f28d915b69e0d0dfb5dcb128298237510106f0b1
pold500 <paveld500@gmail.com>
2016-12-13 16:25:45 +0200

    Fix cmd format

--- WinsockServer/UserCommandNoOperation.cpp
+++ WinsockServer/UserCommandNoOperation.cpp
@@ -5,7 +5,7 @@
 void UserCommandNoOperation::execute()
 {
 	static const std::string no_such_command_message = "No such command exist. " +
-		std::string("List of available commands: list, get polygons [start_range-end_range], get polygons [indexes] \n");
+		std::string("List of available commands: list, get polygons [start_range..end_range], get polygons [indexes] \n");
 	Helpers::sendPacket(m_ClientSocket, no_such_command_message);
 }
 
